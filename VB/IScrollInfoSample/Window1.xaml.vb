8e329258df0f4c624e2e65b7bb0a97a0bbb923d5
DevExpressExampleBot <fakeEmail>
2022-12-16 17:42:15 +0400

    Source auto update [skip ci]

--- VB/IScrollInfoSample/Window1.xaml.vb
+++ VB/IScrollInfoSample/Window1.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Windows
@@ -6,50 +6,46 @@ Imports System.Windows.Controls
 Imports DevExpress.Mvvm.UI
 
 Namespace Sample
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
             Dim list = New List(Of Item)()
-            For i As Integer = 0 To 99
-                list.Add(New Item() With { _
-                    .Number = i Mod 3, _
-                    .Text = "row" & i _
-                })
-            Next i
-            grid.ItemsSource = list
+            For i As Integer = 0 To 100 - 1
+                list.Add(New Item() With {.Number = i Mod 3, .Text = "row" & i})
+            Next
+
+            Me.grid.ItemsSource = list
         End Sub
 
         Public Sub GetVisibleRowsOnScreen()
-            listBox.Items.Clear()
-
-            Dim scrollViewer = LayoutTreeHelper.GetVisualChildren(view).OfType(Of ScrollViewer)().FirstOrDefault()
-            If scrollViewer Is Nothing Then
-                Return
-            End If
-
+            Me.listBox.Items.Clear()
+            Dim scrollViewer = LayoutTreeHelper.GetVisualChildren(Me.view).OfType(Of ScrollViewer)().FirstOrDefault()
+            If scrollViewer Is Nothing Then Return
             Dim bottomIndex As Integer = Convert.ToInt32(scrollViewer.ViewportHeight + scrollViewer.VerticalOffset)
-
-            For i As Integer = grid.View.TopRowIndex To bottomIndex
-                Dim handle = grid.GetRowHandleByVisibleIndex(i)
-                If Not grid.IsValidRowHandle(handle) Then
-                    Continue For
-                End If
-                Dim item = TryCast(grid.GetRow(handle), Item)
+            For i As Integer = Me.grid.View.TopRowIndex To bottomIndex
+                Dim handle = Me.grid.GetRowHandleByVisibleIndex(i)
+                If Not Me.grid.IsValidRowHandle(handle) Then Continue For
+                Dim item = TryCast(Me.grid.GetRow(handle), Item)
                 If item Is Nothing Then
-                    listBox.Items.Add("<null>")
+                    Me.listBox.Items.Add("<null>")
                 Else
-                    listBox.Items.Add(If(grid.IsGroupRowHandle(handle), "Group: " & item.Text, item.Text))
+                    Me.listBox.Items.Add(If(Me.grid.IsGroupRowHandle(handle), "Group: " & item.Text, item.Text))
                 End If
-            Next i
+            Next
         End Sub
+
         Private Sub OnButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
             GetVisibleRowsOnScreen()
         End Sub
     End Class
+
     Public Class Item
-        Public Property Number() As Integer
-        Public Property Text() As String
+
+        Public Property Number As Integer
+
+        Public Property Text As String
     End Class
 End Namespace
